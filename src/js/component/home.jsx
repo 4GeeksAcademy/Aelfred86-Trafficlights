788a5ce4ec811b287a32da6404e3cfa51aa1238f
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-03-15 23:09:56 +0000

    mis cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,9 +5,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [color, setColor] = useState("")
-	const [color1, setColor1] = useState(false)
-	const [color2, setColor2] = useState(false)
+	const [color, setColor] = useState("red")
+	const [bucle, setBucle] = useState(false)
+useEffect(() => {
+if(bucle){
+	setInterval(() => {
+		if(color=="red"){ 
+			setColor("yellow")
+		} else if(color=="yellow"){ 
+			setColor("green")
+		}else if(color=="green"){ 
+			setColor("red")
+		}
+	},1000)
+}
+}, [bucle])
+
 	return (
 		<div className="text-center container">
 			<h1 className="text-center mt-5">Traffic Light</h1>
@@ -20,30 +33,30 @@ const Home = () => {
 
 				<div className="d-flex flex-column bg-dark p-5 justify-content-center align-items-center" style={{ maxWidth: "150px" }}>
 
-					<div onMouseOver={() => setColor("red")} className="btn btn-danger p-5" style={{
-						width: "50px", height: "50px", borderRadius: "50%",
+					<div name="red" onClick={(e) => setColor("red")} className="btn btn-danger p-5 rounded-circle" style={{
+						
 						boxShadow: `${color == "red" ? "0px 0px 100px 50px rgb(255, 34, 0)" : ""}`
-					}} onMouseLeave={() => setColor("")}>
+					}} >
 
 					</div>
 
-					<div onMouseOver={() => setColor1(!color1)} className="btn p-5 mt-3 btn-warning" style={{
-						width: "50px", height: "50px", borderRadius: "50%",
-						boxShadow: `${color1 ? "0px 0px 100px 50px yellow" : ""}`
-					}} onMouseLeave={() => setColor1(false)}>
+					<div name="yellow" onClick={(e) => setColor("yellow")} className="btn p-5 mt-3 btn-warning rounded-circle" style={{
+					
+						boxShadow: `${color == "yellow" ? "0px 0px 100px 50px yellow" : ""}`
+					}} >
 
 					</div>
 
-					<div onMouseOver={() => setColor2(!color1)} className="btn p-5 mt-3 btn-success" style={{
-						width: "50px", height: "50px", borderRadius: "50%",
-						boxShadow: `${color2 ? "0px 0px 100px 50px green" : ""}`
-					}} onMouseLeave={() => setColor2(false)}>
+					<div name="green" onClick={() => setColor("green")} className="btn p-5 mt-3 btn-success rounded-circle" style={{
+					
+						boxShadow: `${color == "green" ? "0px 0px 100px 50px green" : ""}`
+					}} >
 
 					</div>
 
 				</div>
 
-
+<button className="btn btn-primary" onClick={() => setBucle(!bucle)}> Activar bucle </button>
 			</div>
 
 		</div>
